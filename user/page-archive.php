8d593c41ef45e9c6a7384240e66f378d44930e7c
leaphy-dev <leaf443@outlook.com>
2025-06-24 09:48:21 +0800

    修复一处字体引用问题

--- user/page-archive.php
+++ user/page-archive.php
@@ -1,11 +1,11 @@
 <?php 
 /*
-  Template Name: Timearchive Template
+  Template Name: Archive Template
 */
 get_header();
 ?>
 
-<link href="https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@900&display=swap" rel="stylesheet">
+<link href="https://<?= esc_attr(iro_opt('gfonts_api', 'fonts.googleapis.com')); ?>/css?family=Noto+Serif+SC:wght@900&display=swap" rel="stylesheet">
 <style>
     /* 基础布局 */
     .site-content {
