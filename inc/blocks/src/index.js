b46ae6c24a0fea8f4e8d81cc629ee0ef829c5d46
nicocatxzc <nicocatxzc@163.com>
2025-06-25 01:47:33 +0800

    添加对话块

--- inc/blocks/src/index.js
+++ inc/blocks/src/index.js
@@ -1,10 +1,14 @@
 import hljsSupport from './modules/hljs';
 import noticeBlock from './modules/notice'
+import showcardBlock from './modules/showcard'
+import conversationBlock from './modules/converstation'
 
 export default function initBlocks() {
     try {
         hljsSupport();
         noticeBlock();
+        showcardBlock();
+        conversationBlock();
     } catch (error) {
         console.log(`发生错误${error}`)
         console.log(error.stack)
---
+++ inc/blocks/src/modules/converstation.js
@@ -0,0 +1,153 @@
+import { __ } from '@wordpress/i18n';
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps,BlockControls,RichText,MediaUpload,MediaUploadCheck,} from '@wordpress/block-editor';
+import { ToolbarGroup,ToolbarButton,TextControl,} from '@wordpress/components';
+import { Fragment } from '@wordpress/element';
+
+let lang = {};
+switch ((iroBlockEditor.language || window.navigator.language || "zh-CN").replace("_","-")) {
+    case "zh-CN":
+    case "zh-Hans":
+        lang = {
+            blockTitle: "对话块",
+            imageLabel: "设置头像",
+            directionLabel: "切换方向",
+            placeholder: "请输入对话内容…",
+        };
+        break;
+    case "zh-TW":
+    case "zh-HK":
+    case "zh-MO":
+        lang = {
+            blockTitle: "對話區塊",
+            imageLabel: "設定大頭貼",
+            directionLabel: "切換方向",
+            placeholder: "請輸入對話內容…",
+        };
+        break;
+    case "ja":
+    case "ja-JP":
+        lang = {
+            blockTitle: "会話ブロック",
+            imageLabel: "アバター設定",
+            directionLabel: "方向切替",
+            placeholder: "ここに会話内容を入力…",
+        };
+        break;
+    default:
+        lang = {
+            blockTitle: "Conversations Block",
+            imageLabel: "Set Avatar",
+            directionLabel: "Toggle Direction",
+            placeholder: "Enter conversation text…",
+        };
+}
+
+export default function conversationBlock() {
+
+    function edit({ attributes, setAttributes }) {
+        const { avatar, direction, content } = attributes;
+        const blockProps = useBlockProps();
+        // 切换方向
+        const toggleDirection = () => {
+          setAttributes({
+            direction: direction === "row" ? "row-reverse" : "row",
+          });
+        };
+        return (
+            <Fragment>
+                <BlockControls>
+                    <ToolbarGroup>
+
+                        <MediaUploadCheck>
+                            <MediaUpload
+                                onSelect={(media) =>
+                                    setAttributes({ avatar: media.url })
+                                }
+                                allowedTypes={["image"]}
+                                value={avatar}
+                                render={({ open }) => (
+                                    <ToolbarButton
+                                        icon="format-image"
+                                        label={lang.imageLabel}
+                                        onClick={open}
+                                    />
+                                )}
+                            />
+                        </MediaUploadCheck>
+
+                        <ToolbarButton
+                            icon={
+                                direction === "row"
+                                    ? "arrow-right-alt"
+                                    : "arrow-left-alt"
+                            }
+                            label={lang.directionLabel}
+                            onClick={toggleDirection}
+                        />
+                    </ToolbarGroup>
+                    
+                </BlockControls>
+                <div
+                    {...blockProps}
+                    className="conversations-code"
+                    style={{ display: "flex", flexDirection: direction }}
+                >
+                    {avatar ? (
+                        <img src={avatar} alt="" />
+                    ) : (
+                        <TextControl
+                            placeholder={lang.imageLabel + " URL…"}
+                            value={avatar}
+                            onChange={(url) => setAttributes({ avatar: url })}
+                        />
+                    )}
+                    <RichText
+                        tagName="div"
+                        className="conversations-code-text"
+                        placeholder={lang.placeholder}
+                        value={content}
+                        onChange={(value) => setAttributes({ content: value })}
+                    />
+                </div>
+            </Fragment>
+        );
+    }
+
+    registerBlockType("sakurairo/conversations-block", {
+        title: lang.blockTitle,
+        icon: "format-status",
+        category: "common",
+        attributes: {
+            avatar: {
+                type: "string",
+                default: "",
+            },
+            direction: {
+                type: "string",
+                default: "row",
+            },
+            content: {
+                type: "string",
+                source: "html",
+                selector: ".conversations-code-text",
+            },
+        },
+        edit,
+        save({ attributes }) {
+            const { avatar, direction, content } = attributes;
+            return (
+                <div
+                    className="conversations-code"
+                    style={{ display: "flex", flexDirection: direction }}
+                >
+                    {avatar && <img src={avatar} alt="" />}
+                    <div
+                        className="conversations-code-text"
+                        dangerouslySetInnerHTML={{ __html: content }}
+                    />
+                </div>
+            );
+        },
+    });
+}
