cb2f592d233f906e18413ad0a8f9a4ef377913da
nicocatxzc <nicocatxzc@gmail.com>
2026-09-22 22:27:00 +0800

    feat:封面高度

--- frontend/components/homepage/cover.php
+++ frontend/components/homepage/cover.php
@@ -1,64 +1,68 @@
-<div class="homepage-cover <?= is_home() ? '' : 'hide' ?>">
-    <figure
-        class="cover
+<?php if (iro_opt("cover_switch", true) != false && iro_opt('cover_height', 100) != 0) : ?>
+    <div class="homepage-cover <?= is_home() ? '' : 'hide' ?>"
+        style="
+        --cover-height: <?= iro_opt('cover_height', 100) ?>dvh; ">
+        <figure
+            class="cover
         <?= iro_opt('cover_as_background', false) ? 'transparent' : '' ?>
         "
-        style="
+            style="
         --background-img-pc: url(<?= iro_opt('cover_random_pic_url_pc') ?>); 
         --background-img-mb: url(<?= iro_opt('cover_random_pic_url_mb') ?>);">
-        <div class="cover-info">
-            <div class="center">
-                <?php if (iro_opt("cover_focus_style") != "off") : ?>
-                    <?php if (iro_opt("cover_focus_style", "text") == "avatar"): ?>
-                        <img
-                            src="<?= iro_opt('cover_avatar') ?>"
-                            class="nuxtpic cover-avatar" />
-                    <?php else: ?>
-                        <h1
-                            class="cover-title"
-                            style="
+            <div class="cover-info">
+                <div class="center">
+                    <?php if (iro_opt("cover_focus_style") != "off") : ?>
+                        <?php if (iro_opt("cover_focus_style", "text") == "avatar"): ?>
+                            <img
+                                src="<?= iro_opt('cover_avatar') ?>"
+                                class="nuxtpic cover-avatar" />
+                        <?php else: ?>
+                            <h1
+                                class="cover-title"
+                                style="
                                 font-family: <?= iro_opt("cover_title")["font"] ?>;
                                 font-size: <?= iro_opt("cover_title")["size"] ?? 5 ?>rem;
                                 color: <?= iro_opt("cover_title")["color"] ?? "#FFF" ?>rem;
                             ">
-                            <?= iro_opt("cover_title")["text"] ?>
-                        </h1>
+                                <?= iro_opt("cover_title")["text"] ?>
+                            </h1>
+                        <?php endif; ?>
                     <?php endif; ?>
-                <?php endif; ?>
 
-                <?php if (iro_opt("cover_infor_bar_switch", true)): ?>
-                    <div class="socials" style="
+                    <?php if (iro_opt("cover_infor_bar_switch", true)): ?>
+                        <div class="socials" style="
                         board-radius:<?= iro_opt("cover_infor_bar_radius", 1) ?>rem;
                         ">
-                        <?php if (iro_opt("cover_typedjs")): ?>
-                            <div
-                                class="typed-container">
-                                <?php if (iro_opt("cover_typedjs_mark")): ?>
-                                    <i class="fa-solid fa-quote-left"></i>
-                                <?php endif; ?>
-                                <span id="typed" class="typed">
-                                    <?= iro_opt("cover_typedjs_placeholder") ?>
-                                </span>
-                                <?php if (iro_opt("cover_typedjs_mark")): ?>
-                                    <i class="fa-solid fa-quote-right"></i>
-                                <?php endif; ?>
-                            </div>
-                        <?php endif; ?>
-                        <?php if (iro_opt("cover_signature")["text"]): ?>
-                            <div class="signature">
-                                <p
-                                    style="
-                                            font-family:<?= iro_opt("cover_signature")["font"] ?? ""?>;
+                            <?php if (iro_opt("cover_typedjs")): ?>
+                                <div
+                                    class="typed-container">
+                                    <?php if (iro_opt("cover_typedjs_mark")): ?>
+                                        <i class="fa-solid fa-quote-left"></i>
+                                    <?php endif; ?>
+                                    <span id="typed" class="typed">
+                                        <?= iro_opt("cover_typedjs_placeholder") ?>
+                                    </span>
+                                    <?php if (iro_opt("cover_typedjs_mark")): ?>
+                                        <i class="fa-solid fa-quote-right"></i>
+                                    <?php endif; ?>
+                                </div>
+                            <?php endif; ?>
+                            <?php if (iro_opt("cover_signature")["text"]): ?>
+                                <div class="signature">
+                                    <p
+                                        style="
+                                            font-family:<?= iro_opt("cover_signature")["font"] ?? "" ?>;
                                             font-size:<?= iro_opt("cover_signature")["size"] ?? 1 ?>rem;
                                         ">
-                                    <?= iro_opt("cover_signature")["text"] ?>
-                                </p>
-                            </div>
-                        <?php endif; ?>
-                        <?php require_once get_template_directory() . '/frontend/components/homepage/social_links.php'; ?>
-                    </div>
-                <?php endif; ?>
+                                        <?= iro_opt("cover_signature")["text"] ?>
+                                    </p>
+                                </div>
+                            <?php endif; ?>
+                            <?php require_once get_template_directory() . '/frontend/components/homepage/social_links.php'; ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
             </div>
-        </div>
-    </figure>
-</div>
+        </figure>
+    </div>
+<?php endif; ?>
